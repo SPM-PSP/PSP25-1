832ce233e63c042c0ea311493e879868abbb35b3
syyhdadad <163142882+syyhdadad@users.noreply.github.com>
2025-03-26 16:50:27 +0800

    删除

--- codes/eval/gen_glyph.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python eval/render_glyph_imgs.py \
-        --json_path /data/vdb/yuxiang.tyx/AIGC/data/laion_word/test1k.json \
-        --output_dir /data/vdb/yuxiang.tyx/AIGC/data/laion_word/glyph_laion
-        
